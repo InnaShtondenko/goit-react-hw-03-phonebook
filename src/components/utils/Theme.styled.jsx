efe6a7ce4231c76704678f2b0219159602577b40
InnaShtondenko <inna.mycase@gmail.com>
2022-11-08 23:33:21 +0200

    Update Theme.styled.jsx

--- src/components/utils/Theme.styled.jsx
+++ src/components/utils/Theme.styled.jsx
@@ -1,18 +1,16 @@
 export const theme = {
   borders: {
-    small: '1px solid #c7c3c3',
+    small: '1px solid',
   },
   colors: {
-    accent: '#0ab6cd',
+    accentPrimary: '#1c62bc',
+    accentSecondary: '#4f79cd',
+    textColor: '#fbe13a',
+    textColorSecondary: '#2663b3',
     light: '#ffffff',
     dark: '#000000',
-    grey: '#b8b6b6',
-    backgroundLight: '#f2f5f8',
-    backgroundAccent: '#f3b927',
-    statusCode: {
-      green: '#047e04',
-      red: '#ff0000',
-    },
+    grey: '#7d7e7e',
+    warning: '#ff0000',
   },
   fontSizes: [12, 14, 16, 20, 24, 32, 48, 64],
   fontWeights: {
@@ -23,16 +21,23 @@ export const theme = {
   radius: {
     none: '0',
     normal: '5px',
+    big: '8px',
     round: '50%',
   },
   sizes: {
-    cardSmall: '200px',
-    cardNormal: '300px',
-    cardBig: '500px',
+    small: '200px',
+    normal: '300px',
+    wide: '500px',
   },
   space: [0, 4, 8, 16, 32, 64, 128, 256, 512],
   shadows: {
-    small: '3px 5px 9px 0px #676565',
-    medium: '2px 10px 20px #676565',
+    buttonInset: 'inset 0px 1px 8px 0px #89878795',
+    inputInset: 'inset 0px 2px 4px 0px #00000040',
+    inputInsetWarning: 'inset 0px 2px 6px 0px #ff0000',
+    small: '3px 5px 9px 0px #ada6a6',
+    medium: '2px 10px 20px #918c8c',
+  },
+  transitions: {
+    normal: '250ms ease-in-out',
   },
 };
